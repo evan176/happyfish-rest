773798cbf2f6c1b6358a26367b8cf5f3401129ee
Evan Gui <evan176@hotmail.com>
2015-10-10 18:57:12 +0800

    Create test model

--- happyfish/app/models.py
+++ happyfish/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Aquarium(models.Model):
+    updated_time = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    ph = models.FloatField()
+
+    class Meta:
+        ordering = ('updated_time',)
